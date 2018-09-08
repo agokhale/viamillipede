dda8d98f1d4cf3e4272718e4b30b061cf323ada6
agokhale <ash@aeria.net>
2018-09-08 10:45:29 -0400

    structure

--- smoke.sh
+++ smoke.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -x
 
-echo asdf | ./viamillipede rx 4545 tx localhost 4545 verbose 5 threads 3
-dd if=/dev/zero bs=1g count=3 | cat - > /dev/null
-dd if=/dev/zero bs=1g count=3 | ./viamillipede  threads 2  verbose 3 rx 4545 tx localhost 4545  > /dev/null
+echo asdf:
+echo asdf | ./viamillipede rx 4545 tx localhost 4545 verbose 15 threads 3
 
-#tunnel ssh over an fdx viamillipede; this is probably a bad idea as ssh issues tinygrams and the system shoe shines around buffer sync trouble
+echo push zeros around 
+dd if=/dev/zero bs=1g count=3 | cat - > /dev/null
+
+
+echo tunnel ssh over an fdx viamillipede this is probably a bad idea as ssh issues tinygrams and the system shoe shines around buffer sync trouble
 ver="3"
 ./viamillipede charmode threads 2 initiate localhost 22  tx localhost 4546 rx 4545  verbose $ver &
 nitpid=$!
@@ -14,7 +17,7 @@ sleep 3
 ssh  -p 6622 localhost "dd if=/dev/zero bs=10m count=1" > /dev/null
 
 
-#tunnel viamillipede over multiple ssh tunnels; this works handily and will happily boil your cpus doing parallel ssh
+echo tunnel viamillipede over multiple ssh tunnels this works handily and will happily boil your cpus doing parallel ssh expect 340mbps
 ssh -N  -L 6661:localhost:4545 localhost  & 
 sshpis=$!
 ssh -N  -L 6662:localhost:4545 localhost  & 
@@ -22,4 +25,8 @@ sshpis=$!
 viamillipede verbose $ver  rx 4545 > /dev/null &
 rxpid=!
 sleep 1
-dd if=/dev/zero bs=1g count=30 | viamillipede tx localhost 6661 tx localhost 6662 verbose $ver threads 2
+echo zeros via twin parallel ssh port forward
+dd if=/dev/zero bs=1g count=10 | viamillipede tx localhost 6661 tx localhost 6662 verbose $ver threads 2
+
+echo zeros over viamillipede, hot path test
+dd if=/dev/zero bs=1g count=30 | ./viamillipede threads 2 verbose 3 rx 4545 tx localhost 4545  > /dev/null
