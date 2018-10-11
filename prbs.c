aedf5b8d682237ca7e3558cc7701aba24e53d6a9
agokhale <ash@aeria.net>
2018-10-10 21:49:37 -0400

    add

---
+++ prbs.c
@@ -0,0 +1,21 @@
+#include "worker.h"
+// psudorandom generator suitable for no other pupose than to amuse me
+#define salt 0x42F0E1EBA9EA3693 // from ECMA 182
+#define prbs_sequence (permutation ^ (salt * i))
+unsigned long *prbs_gen(unsigned long *payload, unsigned long permutation,
+                        size_t size) {
+  unsigned long *return_payload = payload;
+  for (unsigned long i = 0; i < (size / sizeof(unsigned long)); i++) {
+    payload[i] = prbs_sequence;
+  }
+  return (return_payload);
+}
+int prbs_verify(unsigned long *payload, unsigned long permutation,
+                size_t size) {
+  // exit 1 if  the payload verifies
+  for (unsigned long i = 0; i < (size / sizeof(unsigned long)); i++) {
+    if (payload[i] != prbs_sequence)
+      return (0);
+  }
+  return (1);
+}
