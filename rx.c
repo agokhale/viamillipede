6046cd282b89c37c9c9058c57920215522622157
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-08-20 22:30:27 -0400

    spelling: initialize
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- rx.c
+++ rx.c
@@ -221,7 +221,7 @@ void rxlaunchworkers(struct rxconf_s *rxconf) {
   int worker_cursor = 0;
   int retcode;
   rxconf->done_mbox = 0;
-  rxconf->next_leg = 0; // initalize sequencer
+  rxconf->next_leg = 0; // initialize sequencer
   setvbuf( stdout, NULL, _IONBF, 0);
   if (tcp_recieve_prep(&(rxconf->sa), &(rxconf->socknum), rxconf->port) != 0) {
     whisper(1, "rx: tcp prep failed. this is unfortunate. ");
